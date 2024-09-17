5ebff07c715b07f86aed858346d2fd580fba2b21
Nikolay Toshev <n.toshev@proton.me>
2024-09-17 17:07:04 +0300

    create db modified tables

--- db_setup.py
+++ db_setup.py
@@ -1,15 +1,18 @@
 import sqlalchemy as sa
-from sqlalchemy import values, column
+from sqlalchemy.orm import declarative_base
+
+# Define Table Classes
+Base = declarative_base()
 
 engine = sa.create_engine('sqlite:///work_schedule.db')
 
-class Employee(sa.Base):
+class Employee(Base):
     __tablename__ = 'employees'
 
     id = sa.Column(sa.Integer, primary_key=True)
     name = sa.Column(sa.String)
 
-class WorkingHours(sa.Base):
+class WorkingHours(Base):
     __tablename__ = 'working_hours'
 
     id = sa.Column(sa.Integer, primary_key=True)
@@ -17,13 +20,13 @@ class WorkingHours(sa.Base):
     end_hour = sa.Column(sa.Time)
     working_hours = sa.Column(sa.Float)
 
-class WeekSchedule(sa.Base):
+class WeekSchedule(Base):
     __tablename__ = 'week_schedule'
-
-    MY_VALUES = [(1, 'Понеделник'), (2, 'Вторник'), (3, 'Сряда'), (4, 'Четвъртък'), (5, 'Петък'), (6, 'Събота'), (7, 'Неделя')]
 
     id = sa.Column(sa.Integer, primary_key=True)
     week = sa.Column(sa.String)
-    weekday = sa.Column(sa.Integer, value_expr = values(column('id', sa.Integer), column('name', sa.String), name="my_values").data(MY_VALUES), nullable=False)
-    employee = sa.Column(sa.Integer, sa.ForeignKey('employees.id'), nullable=False)
+    weekday = sa.Column(sa.String)
+    employee = sa.Column(sa.Integer, sa.ForeignKey('employees.id'), nullable=False, default=1)
     working_hours = sa.Column(sa.Integer, sa.ForeignKey('working_hours.id'), nullable=False)
+
+Base.metadata.create_all(engine)
